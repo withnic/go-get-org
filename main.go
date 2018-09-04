ec271ee9ea1ea07dfaaba8d687ce92fd17991b45
micnncim <micnncim@gmail.com>
2018-09-04 15:03:17 +0900

    Use google/go-github

--- main.go
+++ main.go
@@ -1,79 +1,140 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"errors"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"time"
 
+	"github.com/google/go-github/github"
 	"github.com/urfave/cli"
+	"golang.org/x/oauth2"
 )
 
 const (
-	baseURL = "https://api.github.com/orgs/%s/repos?access_token=%s"
+	perpage = 100
 )
 
-type Repos []struct {
-	FullName string `json:"full_name"`
-}
+const (
+	timeout = 30 * time.Second
+)
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "go-get-org"
 	app.Usage = "go get all repositories of the organization"
-	app.Action = GetRepos
+	app.Action = Run
 
 	app.Run(os.Args)
 }
 
-func GetRepos(c *cli.Context) error {
+func Run(c *cli.Context) error {
 	args := c.Args()
 	if len(args) != 2 {
-		log.Fatal("should be 2 args")
+		log.Fatal("args must be organization and token")
 	}
 	org := args[0]
 	token := args[1]
-	url := fmt.Sprintf(baseURL, org, token)
 
-	res, err := http.Get(url)
+	repos, err := getRepos(org, token)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	repos := Repos{}
-	if err := json.NewDecoder(res.Body).Decode(&repos); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	names := make([]string, 0, len(repos))
+	if err := goGetRepos(repos); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return nil
+}
+
+func goGetRepos(repos []*github.Repository) error {
+	out := make(chan string)
+	rest := make([]string, 0, len(repos))
 	for _, repo := range repos {
-		names = append(names, repo.FullName)
+		go func(repo *github.Repository) error {
+			name := filepath.Join("github.com", *repo.FullName, "...")
+			o, err := exec.Command("go", "get", "-u", name).Output()
+			if err != nil {
+				return err
+			}
+			out <- string(o)
+			return nil
+		}(repo)
+		select {
+		case <-out:
+			fmt.Printf("Installing %s SUCCEEDED\n", *repo.FullName)
+		case <-time.After(timeout):
+			fmt.Printf("Installing %s timeout\n", *repo.FullName)
+			rest = append(rest, *repo.FullName)
+		}
 	}
 
-	for {
-		output := make(chan string)
-		for _, name := range names {
-			go func(name string) {
-				path := fmt.Sprintf("github.com/%s/...", name)
-				fmt.Printf("go get -u %s\n", path)
-				out, err := exec.Command("go", "get", "-u", path).Output()
-				if err != nil {
-					log.Fatal(err)
-				}
-				output <- string(out)
-			}(name)
-			select {
-			case <-output:
-				fmt.Printf("go get github.com/%s succeeded\n", name)
-			case <-time.After(30 * time.Second):
-				fmt.Println("timeout")
-			}
-		}
-
-		fmt.Println("done")
+	fmt.Printf("\ninstalled repositories: %d\n", len(repos)-len(rest))
+	fmt.Printf("installed repositories: %d\n", len(rest))
+	if len(rest) == 0 {
 		return nil
 	}
+
+	fmt.Printf("\nthe following repositories not installed\n")
+	for _, r := range rest {
+		fmt.Println(r)
+	}
+	return nil
+}
+
+func getRepos(org, token string) ([]*github.Repository, error) {
+	client, err := NewClient(token)
+	if err != nil {
+		return nil, err
+	}
+
+	var repos []*github.Repository
+	page := 1
+	for {
+		rs, _, err := client.Repositories.ListByOrg(
+			context.Background(),
+			org,
+			&github.RepositoryListByOrgOptions{
+				ListOptions: github.ListOptions{
+					Page:    page,
+					PerPage: perpage,
+				},
+			})
+		if err != nil {
+			return nil, err
+		}
+		if len(rs) == 0 {
+			break
+		}
+		for _, r := range rs {
+			repos = append(repos, r)
+		}
+		page++
+	}
+
+	fmt.Printf("installing the following repositories (%d)\n", len(repos))
+	for _, repo := range repos {
+		fmt.Println(*repo.FullName)
+	}
+	return repos, nil
+}
+
+func NewClient(token string) (*github.Client, error) {
+	if len(token) == 0 {
+		return nil, errors.New("token is missing")
+	}
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(context.Background(), ts)
+
+	return github.NewClient(tc), nil
 }
